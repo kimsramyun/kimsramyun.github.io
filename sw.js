0720c0814bf506f52dcd0849c4b4b44862a5acc0
kye71007 <kye71007@gmail.com>
2026-07-27 14:04:28 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='kims-margin-v1';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const cp=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,cp));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
